94e9bc61bd3d045846600532c46fc0ae17e3ecb1
tje3d <tje3d@yahoo.com>
2017-05-07 02:12:21 +0430

    Add Resize Keyboard Option to ReplyKeyboardMarkup

--- src/Markups/ReplayKeyboardMarkup.php
+++ src/Markups/ReplayKeyboardMarkup.php
@@ -5,19 +5,29 @@ namespace Tje3d\Telegram\Markups;
 use Tje3d\Telegram\Buttons\KeyboardButton;
 use Tje3d\Telegram\Contracts\KeyboardMarkup;
 use Tje3d\Telegram\Traits\MarkupKeyboard;
-use \Closure;
 
 class ReplayKeyboardMarkup extends Markup implements KeyboardMarkup
 {
-	use MarkupKeyboard;
+    use MarkupKeyboard;
 
-	public function buttonType()
-	{
-		return KeyboardButton::class;
-	}
+    public function buttonType()
+    {
+        return KeyboardButton::class;
+    }
 
-	public function to_array()
-	{
-		return ['keyboard' => $this->buttons];
-	}
+    /**
+     * Optional. Requests clients to resize the keyboard vertically for optimal fit (e.g., make the keyboard smaller if there are just two rows of buttons). Defaults to false, in which case the custom keyboard is always of the same height as the app's standard keyboard.
+     */
+    public function resizeKeyboard($value)
+    {
+        return $this->setConfig('resize_keyboard', $value);
+    }
+
+    public function toArray()
+    {
+        return [
+            'keyboard'        => $this->buttons,
+            'resize_keyboard' => $this->getConfig('resize_keyboard', false)
+        ];
+    }
 }
